92cf229448999e532d62b54008b64ec46f602341
Milan Lesichkov <lesichkovm@gmail.com>
2021-04-02 07:18:01 +0100

    Update attribute.go

--- attribute.go
+++ attribute.go
@@ -45,14 +45,30 @@ func (a *Attribute) GetInt() (int, error) {
 }
 
 // GetFloat returns the value as float
-func (a *Attribute) GetFloat() (float32, error) {
-	f64Value, err := strconv.ParseFloat(a.AttributeValue, 32)
-	return float32(f64Value), err
+func (a *Attribute) GetFloat() (float64, error) {
+	f64Value, err := strconv.ParseFloat(a.AttributeValue, 100)
+	return f64Value, err
+	//f64Value, err := strconv.ParseFloat(a.AttributeValue, 32)
+	//return float32(f64Value), err
 }
 
 // GetString returns the value as string
 func (a *Attribute) GetString() string {
 	return a.AttributeValue
+}
+
+// SetFloat sets a float value
+func (a *Attribute) SetFloat(value float64) bool {
+	a.AttributeValue = strconv.FormatFloat(value, 'f', 30, 64)
+
+	return true
+}
+
+// SetInt sets a int value
+func (a *Attribute) SetInt(value int64) bool {
+	a.AttributeValue = strconv.FormatInt(value, 10)
+
+	return true
 }
 
 // SetInterface serializes the values
