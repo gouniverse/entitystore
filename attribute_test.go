b32055c9af40a31002c9882a1368c7d76e73846f
Milan Lesichkov <lesichkovm@gmail.com>
2021-04-02 07:23:57 +0100

    Create attribute_test.go

---
+++ attribute_test.go
@@ -0,0 +1,116 @@
+package entitystore
+
+import (
+	//"log"
+	"log"
+	"testing"
+	//"database/sql"
+	// _ "github.com/mattn/go-sqlite3"
+	// "gorm.io/driver/sqlite"
+	// "gorm.io/gorm"
+)
+
+func TestAttributeCreate(t *testing.T) {
+	db := InitDB("entity_create.db")
+
+	store := NewStore(WithGormDb(db), WithEntityTableName("cms_entity"), WithAttributeTableName("cms_attribute"), WithAutoMigrate(true))
+
+	isOk := store.AttributeSetString("default", "hello", "world")
+
+	if isOk == false {
+		t.Fatalf("Attribute could not be created")
+	}
+}
+
+func TestAttributeString(t *testing.T) {
+	db := InitDB("entity_create.db")
+
+	store := NewStore(WithGormDb(db), WithEntityTableName("cms_entity"), WithAttributeTableName("cms_attribute"), WithAutoMigrate(true))
+
+	isOk := store.AttributeSetString("default", "hello", "world")
+
+	if isOk == false {
+		t.Fatalf("Attribute could not be created")
+	}
+
+	attr := store.AttributeGet("default", "hello")
+
+	if attr == nil {
+		t.Fatalf("Attribute could not be retrieved")
+	}
+
+	if attr.GetString() != "world" {
+		t.Fatalf("Attribute value incorrect")
+	}
+}
+
+func TestAttributeInt(t *testing.T) {
+	db := InitDB("entity_create.db")
+
+	store := NewStore(WithGormDb(db), WithEntityTableName("cms_entity"), WithAttributeTableName("cms_attribute"), WithAutoMigrate(true))
+
+	isOk := store.AttributeSetInt("default", "test_int", 12)
+
+	if isOk == false {
+		t.Fatalf("Attribute could not be created")
+	}
+
+	attr := store.AttributeGet("default", "test_int")
+
+	if attr == nil {
+		t.Fatalf("Attribute could not be retrieved")
+	}
+
+	v, _ := attr.GetInt()
+	if v != 12 {
+		t.Fatalf("Attribute value incorrect")
+	}
+}
+
+func TestAttributeFloat(t *testing.T) {
+	db := InitDB("entity_create.db")
+
+	store := NewStore(WithGormDb(db), WithEntityTableName("cms_entity"), WithAttributeTableName("cms_attribute"), WithAutoMigrate(true))
+
+	isOk := store.AttributeSetFloat("default", "test_float", 12.123456789123456789123456789)
+
+	if isOk == false {
+		t.Fatalf("Attribute could not be created")
+	}
+
+	attr := store.AttributeGet("default", "test_float")
+
+	if attr == nil {
+		t.Fatalf("Attribute could not be retrieved")
+	}
+
+	v, _ := attr.GetFloat()
+	if v != 12.123456789123456789123456789 {
+		t.Fatalf("Attribute value incorrect")
+	}
+}
+
+func TestAttributeInterface(t *testing.T) {
+	db := InitDB("entity_create.db")
+
+	store := NewStore(WithGormDb(db), WithEntityTableName("cms_entity"), WithAttributeTableName("cms_attribute"), WithAutoMigrate(true))
+
+	isOk := store.AttributeSetInterface("default", "test_interface", "Hello world")
+
+	if isOk == false {
+		t.Fatalf("Attribute could not be created")
+	}
+
+	attr := store.AttributeGet("default", "test_interface")
+
+	if attr == nil {
+		t.Fatalf("Attribute could not be retrieved")
+	}
+
+	//v,_:=attr.GetFloat()
+	v := attr.GetString()
+	log.Println(v);
+	// if v != 12.123456789123456789123456789 {
+	// 	t.Fatalf("Attribute value incorrect")
+	// }
+}
