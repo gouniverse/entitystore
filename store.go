42aced64d052adb50d4679113341cef44a639c8d
Milan Lesichkov <lesichkovm@gmail.com>
2021-04-02 05:43:44 +0100

    Update store.go

--- store.go
+++ store.go
@@ -1,6 +1,7 @@
 package entitystore
 
 import (
+	"errors"
 	"log"
 
 	"gorm.io/driver/sqlite"
@@ -59,28 +60,6 @@ func WithAttributeTableName(attributeTableName string) StoreOption {
 	}
 }
 
-// // NewStore creates a new entity store
-// func NewStore(driverName string, dsn string, entityTableName string, attributeTableName string) *Store {
-// 	log.Println("New entity store: " + dsn)
-
-// 	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
-
-// 	if err != nil {
-// 		panic("failed to connect database")
-// 	}
-
-// 	db.Table(entityTableName).AutoMigrate(&entity{})
-// 	db.Table(attributeTableName).AutoMigrate(&attribute{})
-
-// 	st := &Store{
-// 		db:                 db,
-// 		entityTableName:    entityTableName,
-// 		attributeTableName: attributeTableName,
-// 	}
-
-// 	return st
-// }
-
 // NewStore creates a new entity store
 func NewStore(opts ...StoreOption) *Store {
 	store := &Store{}
@@ -105,22 +84,335 @@ func NewStore(opts ...StoreOption) *Store {
 
 // AutoMigrate auto migrate
 func (st *Store) AutoMigrate() {
-	st.db.Table(st.entityTableName).AutoMigrate(&entity{})
-	st.db.Table(st.attributeTableName).AutoMigrate(&attribute{})
+	st.db.Table(st.entityTableName).AutoMigrate(&Entity{})
+	st.db.Table(st.attributeTableName).AutoMigrate(&Attribute{})
 }
 
-// NewStoreGorm creates a new entity store
-// func NewStoreGorm(db *gorm.DB, entityTableName string, attributeTableName string) *Store {
-// 	log.Println("New entity store: " + db.Name())
 
-// 	db.Table(entityTableName).AutoMigrate(&entity{})
-// 	db.Table(attributeTableName).AutoMigrate(&attribute{})
+// EntityCount counts entities
+func (st *Store) EntityCount(entityType string) uint64 {
+	var count int64
+	st.db.Table(st.entityTableName).Where("type=?", entityType).Count(&count)
+	return uint64(count)
+}
 
-// 	st := &Store{
-// 		db:                 db,
-// 		entityTableName:    entityTableName,
-// 		attributeTableName: attributeTableName,
-// 	}
+// EntityCreate creates a new entity
+func (st *Store) EntityCreate(entityType string) *Entity {
+	entity := &Entity{Type: entityType, Status: "active", st: st}
 
-// 	return st
-// }
+	dbResult := st.db.Table(st.entityTableName).Create(&entity)
+
+	if dbResult.Error != nil {
+		return nil
+	}
+
+	return entity
+}
+
+// EntityCreateWithAttributes func
+func (st *Store) EntityCreateWithAttributes(entityType string, attributes map[string]interface{}) *Entity {
+	// Note the use of tx as the database handle once you are within a transaction
+	tx := st.db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		return nil
+	}
+
+	//return tx.Commit().Error
+
+	entity := &Entity{Type: entityType, Status: EntityStatusActive, st: st}
+
+	dbResult := tx.Table(st.entityTableName).Create(&entity)
+
+	if dbResult.Error != nil {
+		tx.Rollback()
+		return nil
+	}
+
+	//entityAttributes := make([]EntityAttribute, 0)
+	for k, v := range attributes {
+		ea := Attribute{EntityID: entity.ID, AttributeKey: k} //, AttributeValue: value}
+		ea.SetInterface(v)
+
+		dbResult2 := tx.Table(st.attributeTableName).Create(&ea)
+		if dbResult2.Error != nil {
+			tx.Rollback()
+			return nil
+		}
+	}
+
+	err := tx.Commit().Error
+
+	if err != nil {
+		tx.Rollback()
+		return nil
+	}
+
+	return entity
+}
+
+// EntityDelete deletes an entity and all attributes
+func (st *Store) EntityDelete(entityID string) bool {
+	if entityID == "" {
+		return false
+	}
+
+	// Note the use of tx as the database handle once you are within a transaction
+	tx := st.db.Begin()
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		log.Println(err)
+		return false
+	}
+
+	if err := tx.Where("entity_id=?", entityID).Table(st.attributeTableName).Delete(&Attribute{}).Error; err != nil {
+		tx.Rollback()
+		log.Println(err)
+		return false
+	}
+
+	if err := tx.Where("id=?", entityID).Table(st.entityTableName).Delete(&Entity{}).Error; err != nil {
+		tx.Rollback()
+		log.Println(err)
+		return false
+	}
+
+	err := tx.Commit().Error
+
+	if err == nil {
+		return true
+	}
+
+	log.Println(err)
+
+	return false
+}
+
+// EntityFindByID finds an entity by ID
+func (st *Store) EntityFindByID(entityID string) *Entity {
+	if entityID == "" {
+		return nil
+	}
+
+	entity := &Entity{}
+
+	resultEntity := st.db.Table(st.entityTableName).First(&entity, "id=?", entityID)
+
+	if resultEntity.Error != nil {
+		if errors.Is(resultEntity.Error, gorm.ErrRecordNotFound) {
+			return nil
+		}
+		log.Panic(resultEntity.Error)
+	}
+
+	// DEBUG: log.Println(entity)
+
+	return entity
+}
+
+// EntityFindByAttribute finds an entity by attribute
+func (st *Store) EntityFindByAttribute(entityType string, attributeKey string, attributeValue string) *Entity {
+	attr := &Attribute{}
+
+	subQuery := st.db.Table(st.entityTableName).Model(&Entity{}).Select("id").Where("type = ?", entityType)
+	result := st.db.Table(st.attributeTableName).First(&attr, "entity_id IN (?) AND attribute_key=? AND attribute_value=?", subQuery, attributeKey, attributeValue)
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil
+	}
+
+	if result.Error != nil {
+		log.Panic(result.Error)
+	}
+
+	// DEBUG: log.Println(entityAttribute)
+
+	ent := &Entity{}
+
+	resultEntity := st.db.Table(st.entityTableName).First(&ent, "id=?", attr.EntityID)
+
+	if errors.Is(resultEntity.Error, gorm.ErrRecordNotFound) {
+		return nil
+	}
+
+	if resultEntity.Error != nil {
+		log.Panic(resultEntity.Error)
+	}
+
+	// DEBUG: log.Println(entity)
+
+	return ent
+}
+
+// EntityList lists entities
+func (st *Store) EntityList(entityType string, offset uint64, perPage uint64, search string, orderBy string, sort string) []Entity {
+	entityList := []Entity{}
+	result := st.db.Table(st.entityTableName).Where("type=?", entityType).Order(orderBy + " " + sort).Offset(int(offset)).Limit(int(perPage)).Find(&entityList)
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil
+	}
+
+	return entityList
+}
+
+// EntityListByAttribute finds an entity by attribute
+func (st *Store) EntityListByAttribute(entityType string, attributeKey string, attributeValue string) []Entity {
+	//entityAttributes := []EntityAttribute{}
+	var entityIDs []string
+
+	subQuery := st.db.Table(st.entityTableName).Model(&Entity{}).Select("id").Where("type = ?", entityType)
+	result := st.db.Table(st.attributeTableName).Model(&Attribute{}).Select("entity_id").Find(&entityIDs, "entity_id IN (?) AND attribute_key=? AND attribute_value=?", subQuery, attributeKey, attributeValue)
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil
+	}
+
+	if result.Error != nil {
+		log.Panic(result.Error)
+	}
+
+	// DEBUG: log.Println(result)
+
+	entities := []Entity{}
+
+	resultEntity := st.db.Table(st.entityTableName).Where("id IN (?)", entityIDs).Find(&entities)
+
+	if errors.Is(resultEntity.Error, gorm.ErrRecordNotFound) {
+		return nil
+	}
+
+	if resultEntity.Error != nil {
+		log.Panic(resultEntity.Error)
+	}
+
+	// DEBUG: log.Println(entity)
+
+	return entities
+}
+
+// AttributeCreate creates a new attribute
+func (st *Store) AttributeCreate(entityID string, attributeKey string, attributeValue string) *Attribute {
+	attr := &Attribute{EntityID: entityID, AttributeKey: attributeKey, AttributeValue: attributeValue}
+
+	dbResult := st.db.Table(st.attributeTableName).Create(&attr)
+
+	if dbResult.Error != nil {
+		return nil
+	}
+
+	return attr
+}
+
+// AttributeFind finds an entity by ID
+func (st *Store) AttributeFind(entityID string, attributeKey string) *Attribute {
+	attr := &Attribute{}
+
+	result := st.db.Table(st.attributeTableName).First(&attr, "entity_id=? AND attribute_key=?", entityID, attributeKey)
+
+	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+		return nil
+	}
+
+	if result.Error != nil {
+		log.Panic(result.Error)
+	}
+
+	return attr
+}
+
+// AttributeGet the name of the User table
+func (st *Store) AttributeGet(entityID string, attributeKey string) *Attribute {
+	attr := &Attribute{}
+
+	result := st.db.Table(st.attributeTableName).First(&attr, "entity_id=? AND attribute_key=?", entityID, attributeKey)
+
+	if result.Error != nil {
+
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return nil
+		}
+
+		log.Panic(result.Error)
+	}
+
+	return attr
+}
+
+// AttributeSet creates a new entity
+func (st *Store) AttributeSet(entityID string, attributeKey string, attributeValue string) bool {
+	attr := st.AttributeFind(entityID, attributeKey)
+
+	if attr == nil {
+		attr = st.AttributeCreate(entityID, attributeKey, attributeValue)
+		if attr != nil {
+			return true
+		}
+		return false
+	}
+
+	attr.AttributeValue = attributeValue
+	dbResult := st.db.Table(st.attributeTableName).Save(attr)
+	if dbResult.Error != nil {
+		return false
+	}
+
+	return true
+}
+
+// AttributesSet upserts and entity attribute
+func (st *Store) AttributesSet(entityID string, attributes map[string]interface{}) bool {
+	tx := st.db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	if err := tx.Error; err != nil {
+		return false
+	}
+
+	for k, v := range attributes {
+		attr := st.AttributeFind(entityID, k)
+
+		if attr == nil {
+			attr = &Attribute{EntityID: entityID, AttributeKey: k}
+			attr.SetInterface(v)
+
+			dbResult := tx.Table(st.attributeTableName).Create(&attr)
+			if dbResult.Error != nil {
+				tx.Rollback()
+				return false
+			}
+
+		}
+
+		attr.SetInterface(v)
+		dbResult := tx.Table(st.attributeTableName).Save(attr)
+		if dbResult.Error != nil {
+			return false
+		}
+	}
+
+	err := tx.Commit().Error
+
+	if err != nil {
+		tx.Rollback()
+		return false
+	}
+
+	return true
+
+}
