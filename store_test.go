b382bd9f01d2dc87649a7ea65b549bf7a4230043
Milan Lesichkov <lesichkovm@gmail.com>
2021-04-03 06:16:30 +0100

    Create store_test.go

---
+++ store_test.go
@@ -0,0 +1,45 @@
+package entitystore
+
+import (
+	//"log"
+	"testing"
+	//"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitDB(filepath string) *gorm.DB /**sql.DB*/ {
+	db, err := gorm.Open(sqlite.Open(filepath), &gorm.Config{})
+
+	if err != nil {
+		panic(err) 
+	}
+	
+	return db
+}
+
+func TestStoreCreate(t *testing.T) {
+	db := InitDB("entity_create.db")
+	
+	store := NewStore(WithGormDb(db),WithEntityTableName("cms_entity"),WithAttributeTableName("cms_attribute"),WithAutoMigrate(true))
+	
+	entity := store.EntityCreate("post")
+	if entity == nil{
+		t.Fatalf("Entity could not be created")
+	}
+}
+
+
+func TestStoreAutomigrate(t *testing.T) {
+	db := InitDB("entity_create.db")
+	
+	store := NewStore(WithGormDb(db),WithEntityTableName("cms_entity"),WithAttributeTableName("cms_attribute"))
+
+	store.AutoMigrate()
+	
+	entity := store.EntityCreate("post")
+	if entity == nil{
+		t.Fatalf("Entity could not be created")
+	}
+}
